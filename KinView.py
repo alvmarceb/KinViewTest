f5a075ae406f3447b1121fdce548ef77147f6d86
Alvaro Martinez <alvmarceb@gmail.com>
2019-08-13 12:07:30 +0200

    Initial commit

---
+++ KinView.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#Este me lo invento y no tiene nada que ver con el otro, a ver qué pasa
+
+#Añado una línea más a ver SmartGit qué hace
+
+#Ahora meto otra línea, que debería dejar obsoleta la rama de antes
